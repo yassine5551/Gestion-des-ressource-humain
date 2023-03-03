31ed2321183fc23bca0c6cbd06df77550857626a
anass <anasskanbou@gmail.com>
2023-03-03 07:46:50 -0300

    create admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('admin.index');
 });
 
 \Illuminate\Support\Facades\Auth::routes(["register"=>false]);
