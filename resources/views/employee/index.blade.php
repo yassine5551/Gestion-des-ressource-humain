3435eb2f3c2b29d2a24f2c3cb8549e8eda1c7e24
ismail ait hsaine <aithsaine3@gmail.com>
2023-03-11 23:09:15 +0100

    update view employees index

--- resources/views/employee/index.blade.php
+++ resources/views/employee/index.blade.php
@@ -53,56 +53,6 @@
 
         <h1 class="m-4">Employes</h1>
 
-        <div id="content">
-        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
-                <th scope="col" class="px-6 py-3">
-                    numero social
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    nom complet
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    post
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    salaire
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    date d'embauch
-                </th>
-            </tr>
-            </thead>
-            <tbody>
-            @foreach($employees as $employer)
-
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-orange-200">
-
-                <td class="px-6 py-4">
-                    {{$employer->getSocialNumber()}}
-                </td>
-                <td class="px-6 py-4">
-                    {{$employer->user->getFirstName()}} {{$employer->user->getLastName()}}
-                </td>
-                <td class="px-6 py-4">
-                    {{$employer->post->getName()}}
-                </td>
-                <td class="px-6 py-4">
-                    {{$employer->getSalary()}} DH
-                </td>
-                <td class="px-6 py-4">
-                    {{$employer->getHiringDate()}}
-                </td>
-                <td class="px-6 py-4">
-                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                </td>
-            </tr>
-            @endforeach
-
-            </tbody>
-        </table>
-    </div>
 
 </div>
 
@@ -111,29 +61,28 @@
                     <thead>
                     <tr>
                         <th></th>
-                        <th>Name</th>
-                        <th>Company</th>
-                        <th>City</th>
-                        <th>Progress</th>
-                        <th>Created</th>
+                        <th>Social number</th>
+                        <th>nom</th>
+                        <th>post</th>
+                        <th>salaire</th>
+                        <th>date d'embauch</th>
                         <th></th>
                     </tr>
                     </thead>
                     <tbody>
-                    <tr>
+                    @foreach($employees as $employer)
+                        <tr>
                         <td class="image-cell">
                             <div class="image">
-                                <img src="https://avatars.dicebear.com/v2/initials/rebecca-bauch.svg" class="rounded-full">
+                                <img src={{"https://avatars.dicebear.com/v2/initials/{$employer->user->getFirstName()}-{$employer->user->getLastName()}.svg"}} class="rounded-full">
                             </div>
                         </td>
-                        <td data-label="Name">Rebecca Bauch</td>
-                        <td data-label="Company">Daugherty-Daniel</td>
-                        <td data-label="City">South Cory</td>
-                        <td data-label="Progress" class="progress-cell">
-                            <progress max="100" value="79">79</progress>
-                        </td>
+                            <td data-label="social number">{{$employer->getSocialNumber()}}</td>
+                            <td data-label="nom">{{$employer->user->getFirstName()}} {{$employer->user->getLastName()}} </td>
+                            <td data-label="post">{{$employer->post->getName()}}</td>
+                            <td data-label="Progress" class="progress-cell">{{$employer->getSalary()."  DH"}}</td>
                         <td data-label="Created">
-                            <small class="text-gray-500" title="Oct 25, 2021">Oct 25, 2021</small>
+                            <small class="text-gray-500" title="Oct 25, 2021">{{$employer->getHiringDate()}}</small>
                         </td>
                         <td class="actions-cell">
                             <div class="buttons right nowrap">
@@ -146,240 +95,8 @@
                             </div>
                         </td>
                     </tr>
-                    <tr>
-                        <td class="image-cell">
-                            <div class="image">
-                                <img src="https://avatars.dicebear.com/v2/initials/felicita-yundt.svg" class="rounded-full">
-                            </div>
-                        </td>
-                        <td data-label="Name">Felicita Yundt</td>
-                        <td data-label="Company">Johns-Weissnat</td>
-                        <td data-label="City">East Ariel</td>
-                        <td data-label="Progress" class="progress-cell">
-                            <progress max="100" value="67">67</progress>
-                        </td>
-                        <td data-label="Created">
-                            <small class="text-gray-500" title="Jan 8, 2021">Jan 8, 2021</small>
-                        </td>
-                        <td class="actions-cell">
-                            <div class="buttons right nowrap">
-                                <button class="button small green --jb-modal"  data-target="sample-modal-2" type="button">
-                                    <span class="icon"><i class="mdi mdi-eye"></i></span>
-                                </button>
-                                <button class="button small red --jb-modal" data-target="sample-modal" type="button">
-                                    <span class="icon"><i class="mdi mdi-trash-can"></i></span>
-                                </button>
-                            </div>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td class="image-cell">
-                            <div class="image">
-                                <img src="https://avatars.dicebear.com/v2/initials/mr-larry-satterfield-v.svg" class="rounded-full">
-                            </div>
-                        </td>
-                        <td data-label="Name">Mr. Larry Satterfield V</td>
-                        <td data-label="Company">Hyatt Ltd</td>
-                        <td data-label="City">Windlerburgh</td>
-                        <td data-label="Progress" class="progress-cell">
-                            <progress max="100" value="16">16</progress>
-                        </td>
-                        <td data-label="Created">
-                            <small class="text-gray-500" title="Dec 18, 2021">Dec 18, 2021</small>
-                        </td>
-                        <td class="actions-cell">
-                            <div class="buttons right nowrap">
-                                <button class="button small green --jb-modal"  data-target="sample-modal-2" type="button">
-                                    <span class="icon"><i class="mdi mdi-eye"></i></span>
-                                </button>
-                                <button class="button small red --jb-modal" data-target="sample-modal" type="button">
-                                    <span class="icon"><i class="mdi mdi-trash-can"></i></span>
-                                </button>
-                            </div>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td class="image-cell">
-                            <div class="image">
-                                <img src="https://avatars.dicebear.com/v2/initials/mr-broderick-kub.svg" class="rounded-full">
-                            </div>
-                        </td>
-                        <td data-label="Name">Mr. Broderick Kub</td>
-                        <td data-label="Company">Kshlerin, Bauch and Ernser</td>
-                        <td data-label="City">New Kirstenport</td>
-                        <td data-label="Progress" class="progress-cell">
-                            <progress max="100" value="71">71</progress>
-                        </td>
-                        <td data-label="Created">
-                            <small class="text-gray-500" title="Sep 13, 2021">Sep 13, 2021</small>
-                        </td>
-                        <td class="actions-cell">
-                            <div class="buttons right nowrap">
-                                <button class="button small green --jb-modal"  data-target="sample-modal-2" type="button">
-                                    <span class="icon"><i class="mdi mdi-eye"></i></span>
-                                </button>
-                                <button class="button small red --jb-modal" data-target="sample-modal" type="button">
-                                    <span class="icon"><i class="mdi mdi-trash-can"></i></span>
-                                </button>
-                            </div>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td class="image-cell">
-                            <div class="image">
-                                <img src="https://avatars.dicebear.com/v2/initials/barry-weber.svg" class="rounded-full">
-                            </div>
-                        </td>
-                        <td data-label="Name">Barry Weber</td>
-                        <td data-label="Company">Schulist, Mosciski and Heidenreich</td>
-                        <td data-label="City">East Violettestad</td>
-                        <td data-label="Progress" class="progress-cell">
-                            <progress max="100" value="80">80</progress>
-                        </td>
-                        <td data-label="Created">
-                            <small class="text-gray-500" title="Jul 24, 2021">Jul 24, 2021</small>
-                        </td>
-                        <td class="actions-cell">
-                            <div class="buttons right nowrap">
-                                <button class="button small green --jb-modal"  data-target="sample-modal-2" type="button">
-                                    <span class="icon"><i class="mdi mdi-eye"></i></span>
-                                </button>
-                                <button class="button small red --jb-modal" data-target="sample-modal" type="button">
-                                    <span class="icon"><i class="mdi mdi-trash-can"></i></span>
-                                </button>
-                            </div>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td class="image-cell">
-                            <div class="image">
-                                <img src="https://avatars.dicebear.com/v2/initials/bert-kautzer-md.svg" class="rounded-full">
-                            </div>
-                        </td>
-                        <td data-label="Name">Bert Kautzer MD</td>
-                        <td data-label="Company">Gerhold and Sons</td>
-                        <td data-label="City">Mayeport</td>
-                        <td data-label="Progress" class="progress-cell">
-                            <progress max="100" value="62">62</progress>
-                        </td>
-                        <td data-label="Created">
-                            <small class="text-gray-500" title="Mar 30, 2021">Mar 30, 2021</small>
-                        </td>
-                        <td class="actions-cell">
-                            <div class="buttons right nowrap">
-                                <button class="button small green --jb-modal"  data-target="sample-modal-2" type="button">
-                                    <span class="icon"><i class="mdi mdi-eye"></i></span>
-                                </button>
-                                <button class="button small red --jb-modal" data-target="sample-modal" type="button">
-                                    <span class="icon"><i class="mdi mdi-trash-can"></i></span>
-                                </button>
-                            </div>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td class="image-cell">
-                            <div class="image">
-                                <img src="https://avatars.dicebear.com/v2/initials/lonzo-steuber.svg" class="rounded-full">
-                            </div>
-                        </td>
-                        <td data-label="Name">Lonzo Steuber</td>
-                        <td data-label="Company">Skiles Ltd</td>
-                        <td data-label="City">Marilouville</td>
-                        <td data-label="Progress" class="progress-cell">
-                            <progress max="100" value="17">17</progress>
-                        </td>
-                        <td data-label="Created">
-                            <small class="text-gray-500" title="Feb 12, 2021">Feb 12, 2021</small>
-                        </td>
-                        <td class="actions-cell">
-                            <div class="buttons right nowrap">
-                                <button class="button small green --jb-modal"  data-target="sample-modal-2" type="button">
-                                    <span class="icon"><i class="mdi mdi-eye"></i></span>
-                                </button>
-                                <button class="button small red --jb-modal" data-target="sample-modal" type="button">
-                                    <span class="icon"><i class="mdi mdi-trash-can"></i></span>
-                                </button>
-                            </div>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td class="image-cell">
-                            <div class="image">
-                                <img src="https://avatars.dicebear.com/v2/initials/jonathon-hahn.svg" class="rounded-full">
-                            </div>
-                        </td>
-                        <td data-label="Name">Jonathon Hahn</td>
-                        <td data-label="Company">Flatley Ltd</td>
-                        <td data-label="City">Billiemouth</td>
-                        <td data-label="Progress" class="progress-cell">
-                            <progress max="100" value="74">74</progress>
-                        </td>
-                        <td data-label="Created">
-                            <small class="text-gray-500" title="Dec 30, 2021">Dec 30, 2021</small>
-                        </td>
-                        <td class="actions-cell">
-                            <div class="buttons right nowrap">
-                                <button class="button small green --jb-modal"  data-target="sample-modal-2" type="button">
-                                    <span class="icon"><i class="mdi mdi-eye"></i></span>
-                                </button>
-                                <button class="button small red --jb-modal" data-target="sample-modal" type="button">
-                                    <span class="icon"><i class="mdi mdi-trash-can"></i></span>
-                                </button>
-                            </div>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td class="image-cell">
-                            <div class="image">
-                                <img src="https://avatars.dicebear.com/v2/initials/ryley-wuckert.svg" class="rounded-full">
-                            </div>
-                        </td>
-                        <td data-label="Name">Ryley Wuckert</td>
-                        <td data-label="Company">Heller-Little</td>
-                        <td data-label="City">Emeraldtown</td>
-                        <td data-label="Progress" class="progress-cell">
-                            <progress max="100" value="54">54</progress>
-                        </td>
-                        <td data-label="Created">
-                            <small class="text-gray-500" title="Jun 28, 2021">Jun 28, 2021</small>
-                        </td>
-                        <td class="actions-cell">
-                            <div class="buttons right nowrap">
-                                <button class="button small green --jb-modal"  data-target="sample-modal-2" type="button">
-                                    <span class="icon"><i class="mdi mdi-eye"></i></span>
-                                </button>
-                                <button class="button small red --jb-modal" data-target="sample-modal" type="button">
-                                    <span class="icon"><i class="mdi mdi-trash-can"></i></span>
-                                </button>
-                            </div>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td class="image-cell">
-                            <div class="image">
-                                <img src="https://avatars.dicebear.com/v2/initials/sienna-hayes.svg" class="rounded-full">
-                            </div>
-                        </td>
-                        <td data-label="Name">Sienna Hayes</td>
-                        <td data-label="Company">Conn, Jerde and Douglas</td>
-                        <td data-label="City">Jonathanfort</td>
-                        <td data-label="Progress" class="progress-cell">
-                            <progress max="100" value="55">55</progress>
-                        </td>
-                        <td data-label="Created">
-                            <small class="text-gray-500" title="Mar 7, 2021">Mar 7, 2021</small>
-                        </td>
-                        <td class="actions-cell">
-                            <div class="buttons right nowrap">
-                                <button class="button small green --jb-modal"  data-target="sample-modal-2" type="button">
-                                    <span class="icon"><i class="mdi mdi-eye"></i></span>
-                                </button>
-                                <button class="button small red --jb-modal" data-target="sample-modal" type="button">
-                                    <span class="icon"><i class="mdi mdi-trash-can"></i></span>
-                                </button>
-                            </div>
-                        </td>
-                    </tr>
+                    @endforeach
+
                     </tbody>
                 </table>
                 <div class="table-pagination">
