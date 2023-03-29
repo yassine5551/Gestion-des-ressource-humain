ffb9827d77757281bbe12725c8891821c174fa3a
anass <anasskanbou@gmail.com>
2023-03-29 23:51:01 +0000

    création de la migration stagiaire

---
+++ database/migrations/2023_03_29_234302_create_stagiaires_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stagiaires', function (Blueprint $table) {
+            $table->id();
+            $table->string("first_name");
+            $table->string("last_name");
+            $table->string("project");
+            $table->date("date_debut");
+            $table->date("date_fin");
+            $table->boolean("status");
+            $table->text("adress");
+            $table->string("phone");
+            $table->date("born_date");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stagiaires');
+    }
+};
