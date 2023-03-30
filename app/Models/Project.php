d82a3450667edc369a1f1fbab42752fe8648e258
anass <anasskanbou@gmail.com>
2023-03-30 02:44:42 +0000

    création de model Project

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,8 +4,32 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
 
 class Project extends Model
 {
     use HasFactory;
+    protected $fillable = ["name"];
+    public function getName()
+    {
+        return $this->attributes["name"];
+    }
+    public function getId()
+    {
+        return $this->attributes["id"];
+    }
+    public function getDescription()
+    {
+        return $this->attributes["description"];
+    }
+    public function stagiaires()
+    {
+        return $this->hasMany(Stagiaire::class,);
+    }
+    public static function validate(Request  $request)
+    {
+        $request->validate([
+            "name" =>'required' 
+        ]);
+    }
 }
