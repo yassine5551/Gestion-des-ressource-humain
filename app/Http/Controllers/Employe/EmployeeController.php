e58efebd15b160c6666a209ac717aa01bc2395d3
elkoujjer yassine <yassin123beta@gmail.com>
2023-04-06 14:48:25 +0000

    fix middleware

---
+++ app/Http/Controllers/Employe/EmployeeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Employe;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class EmployeeController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware(["auth",'employee']);
+    }
+    public function index()
+    {
+        $title = "Employee - Dashboard";
+       
+        return view("employe.index",compact("title"));
+    }
+
+}
--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,7 +63,8 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'admin'=> \App\Http\Middleware\IsAdminMiddeleware::class
+        'admin'=> \App\Http\Middleware\IsAdminMiddeleware::class,
+        'employee'=> \App\Http\Middleware\isEmployeMiddleware::class
     ];
 
 
