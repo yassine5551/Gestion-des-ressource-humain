d218a9b8bb211ae183d52f7f9970a6c5e3ea6f51
anass <anasskanbou@gmail.com>
2023-04-28 15:46:05 +0100

    création landing page

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -36,6 +36,19 @@
 <body>
 
     <div id="app">
+
+        <nav id="navbar-main" class="navbar is-fixed-top">
+            <div class="navbar-brand">
+                <a class="navbar-item mobile-aside-button">
+                    <span class="icon"><i class="mdi mdi-forwardburger mdi-24px"></i></span>
+                </a>
+                <div class="navbar-item">
+                    <div class="control"><input placeholder="Search everywhere..." class="input"></div>
+                </div>
+            </div>
+
+        </nav>
+
         <aside class="aside is-placed-left is-expanded overflow-x-scroll">
             <div class="aside-tools">
                 <div>
